2177624a8511b34792d538d06c1428c44e9c7c3f
rafwin <rafael.wth@web.de>
2013-07-25 17:34:23 +0200

    Adjusted package hierarchy.

---
+++ wicket-async-task-impl/src/main/java/com/blogspot/mydailyjava/wicket/async/task/ITaskManagerHook.java
@@ -0,0 +1,17 @@
+package com.blogspot.mydailyjava.wicket.async.task;
+
+import java.util.concurrent.Future;
+
+/**
+ * A hook into a manager instance. Allows a better reuse of {@link AbstractTaskModel}.
+ */
+public interface ITaskManagerHook {
+
+    String getId();
+
+    Future<?> getFuture();
+
+    Runnable getRunnable();
+
+    void submit(Runnable runnable, boolean cancelExistent);
+}
